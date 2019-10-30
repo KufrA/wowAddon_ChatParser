e59d809203acb0c10b81d5578432b15d150501e9
cabur1 <cabur1@users.noreply.github.com>
2019-10-30 20:27:21 +0100

    Event changes
    Changed parsed event

--- core.lua
+++ core.lua
@@ -1,10 +1,11 @@
 print("Chat Parser loaded!")
-local frame = CreateFrame("FRAME", "EventFrame");
-frame:RegisterEvent("CHAT_MSG_CHANNEL")
+local eventListener = CreateFrame("FRAME", "EventFrame");
+eventListener:RegisterEvent("CHAT_MSG_CHANNEL")
 local function eventHandler(self, event, ...)
-	local message, _, _, _, _, _, _, _, channel = ...;
+	local message = select(1, ...);
+	local channel = select(9, ...);
 	if channel == "LookingForGroup" then 
 		print(message .. channel);
 	end
 end
-frame:SetScript("OnEvent", eventHandler);
+eventListener:SetScript("OnEvent", eventHandler);
